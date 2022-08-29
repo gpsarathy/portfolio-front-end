6d4828247b39eb0e28a96955b7c949795c84df16
Parthasarathy <gpsarathymail@gmail.com>
2022-08-29 21:34:36 +0530

    added base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import solid from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gpsarathy.github.io',
+  base: '/portfolio-front-end',
   integrations: [tailwind(), react(), solid()],
   vite: {
     ssr: {
